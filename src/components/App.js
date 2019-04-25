7cac05990607967af54ba50d5473145f3c802bd8
Łukasz Wiącek <wiacek@wp.pl>
2019-04-25 20:20:21 +0200

    add state and render data in browser

--- src/components/App.js
+++ src/components/App.js
@@ -4,19 +4,22 @@ import Header from "./Header";
 import Form from "./Form";
 import Results from "./Results";
 
+const APIkey = 'd3334eb120bec7fa718c730d966bf414' 
 class App extends Component {
 
 state={
   value: "",
   city: "",
-  Conditions:"",
+  conditions:"",
   detailedConditions:"",
-  latitude:"",
+  sunrise:"",
+  sunset:"",
   temperature:"",
   pressure:"",
   humidity:"",
   wind:"",
   clouds:"",
+  error:false
 }
  
 handleInputForm = (e) =>{
@@ -27,14 +30,61 @@ handleInputForm = (e) =>{
   })
 }
 
-componentDidUpdate = () =>{
+componentDidUpdate(prevProps, prevState) {
+//dać warunek 
+if(prevState.value !== this.state.value) {
 
-handleSubmitForm = (e) => {
-  e.preventDefault()
+ const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&APPID=${APIkey}&units=metric`
 
-  
+
+// fetch(API)
+// .then(response => {
+//   if (response.ok) {
+//     return response
+//   }
+// })
+// .then(response => response.json())
+// .then(data => console.log(data))
+// .catch(err => console.log(err))
+
+// }
+// }
+  fetch(API)
+  .then(response => {
+    if(response.ok){
+      return response}
+    throw Error("City not found")
+  })
+  .then(response => response.json())
+  .then(data => {
+    const time = new Date().toLocaleString()
+    this.setState((state) => ({
+      error: false,
+      date: time,
+      conditions: data.weather.main,
+      detailedConditions: data.weather.description,
+      latitude: data.coord.lat,
+      longitude: data.coord.lon,
+      sunrise: data.sys.sunrise,
+      sunset: data.sys.sunset,
+      temperature: data.main.temp,
+      pressure: data.main.pressure,
+      humidity: data.main.humidity,
+      wind: data.wind.speed,
+      clouds: data.clouds.all,
+      city: state.value
+    }))
+  })
+  .catch(() => {
+    this.setState(prevState => ({
+      error: true,
+      city: prevState.value
+    }))
+  })
+
 }
 }
+
 
 render(){
   return(
@@ -45,7 +95,8 @@ render(){
     value={this.state.value}
     change={this.handleInputForm}
     />
-    <Results/>
+    <Results state={this.state} />
+
     </div>
 
   )
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,21 +1,25 @@
 import React from 'react';
 
-const Results = () => {
+const Results = (props) => {
+
+    const {date,conditions,detailedConditions,latitude,longitude,sunrise,sunset,temperature,pressure,humidity,wind,clouds,city} = props.state
     return ( 
         <>
-        <h2> Current weather conditions for </h2>
-        <h2>Longitude: </h2>
-        <h2>Latitude: </h2>
+        <h2> Current weather conditions for {city} </h2>
+        <h2>Date: {date} </h2>
+        <h2>Longitude: {longitude} </h2>
+        <h2>Latitude: {latitude} </h2>
         <ul>
         
-            <li>Weather conditions: </li>
-            <li>Detailed weather conditions: </li>
-            <li>Latitude: </li>
-            <li>Temperature: '&#176'C</li>
-            <li>Pressure: hPa</li>
-            <li>Humidity: %</li>
-            <li>Wind speed: m/s</li>
-            <li>Clouds: %</li>
+            <li>Weather conditions: {conditions} </li>
+            <li>Detailed weather conditions: {detailedConditions} </li>
+            <li>Sunrise: {sunrise}  </li>
+            <li>Sunset: {sunset}  </li>
+            <li>Temperature: {temperature} &#176;C</li>
+            <li>Pressure: {pressure} hPa</li>
+            <li>Humidity: {humidity} %</li>
+            <li>Wind speed: {wind} m/s</li>
+            <li>Clouds: {clouds} %</li>
             <li></li>
         </ul>
         </>
