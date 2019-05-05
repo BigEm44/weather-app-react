f9c41468c884e781546015f1e23147b37d9445e2
Łukasz Wiącek <wiacek@wp.pl>
2019-05-05 12:08:54 +0200

    Updates

---
+++ precache-manifest.06592a571caae262dd2cf0dd62e6d3d9.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "e56045277c747e146fe86ede98d1540e",
+    "url": "/weather-app-react/static/media/clearSky.e5604527.jpg"
+  },
+  {
+    "revision": "d7197513bbd231bb9875",
+    "url": "/weather-app-react/static/css/main.97be1fb9.chunk.css"
+  },
+  {
+    "revision": "b246a4fc453f9ee8e4e8",
+    "url": "/weather-app-react/static/js/runtime~main.a31b442a.js"
+  },
+  {
+    "revision": "edf26db9c6fc8ca6354e",
+    "url": "/weather-app-react/static/js/2.253d07d0.chunk.js"
+  },
+  {
+    "revision": "aa33c4668578b010290a19c711a8f349",
+    "url": "/weather-app-react/static/media/anemometer.aa33c466.jpg"
+  },
+  {
+    "revision": "fb95fb970145e2f1f7597bc5aea6419a",
+    "url": "/weather-app-react/static/media/thunderstorm.fb95fb97.jpg"
+  },
+  {
+    "revision": "a2f6f64797e359f59df3f2396408f904",
+    "url": "/weather-app-react/static/media/drizzle.a2f6f647.jpg"
+  },
+  {
+    "revision": "9d6a80dd343f0dc2b67cfee84451d53b",
+    "url": "/weather-app-react/static/media/rain.9d6a80dd.jpg"
+  },
+  {
+    "revision": "3a70ab55cec94ce9d4d28a6a027f3837",
+    "url": "/weather-app-react/static/media/snow.3a70ab55.jpg"
+  },
+  {
+    "revision": "d7197513bbd231bb9875",
+    "url": "/weather-app-react/static/js/main.dcac19eb.chunk.js"
+  },
+  {
+    "revision": "80f03ece4dc593a95b11c7bd2c5e7b98",
+    "url": "/weather-app-react/static/media/clouds.80f03ece.jpg"
+  },
+  {
+    "revision": "2982b52ddd11330a7f2c6558ed6ad7d2",
+    "url": "/weather-app-react/static/media/mist.2982b52d.jpg"
+  },
+  {
+    "revision": "63c25cea0d18b2d163aaeb84a0ca07b4",
+    "url": "/weather-app-react/static/media/smoke.63c25cea.jpg"
+  },
+  {
+    "revision": "30c44849fa759529ef7a2b6c324f2ad1",
+    "url": "/weather-app-react/static/media/haze.30c44849.jpg"
+  },
+  {
+    "revision": "3d7b267ffbe4866aeab1a01e9c536e27",
+    "url": "/weather-app-react/static/media/dust.3d7b267f.jpg"
+  },
+  {
+    "revision": "afd4c23bc6287dd6ce4da254595de323",
+    "url": "/weather-app-react/static/media/fog.afd4c23b.jpg"
+  },
+  {
+    "revision": "d0e1fd3662e70bbc7a975d0452e27e62",
+    "url": "/weather-app-react/static/media/squall.d0e1fd36.jpg"
+  },
+  {
+    "revision": "d012d229376e8f4ce211234e8d1565e9",
+    "url": "/weather-app-react/static/media/tornado.d012d229.jpg"
+  },
+  {
+    "revision": "b2160a6fb3532d9e37fa382ddcc44391",
+    "url": "/weather-app-react/index.html"
+  }
+];
